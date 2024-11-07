454b690f018dff10f382f9d7bdf148d174461fdd
Merckerk <marckussantiago924@gmail.com>
2024-11-08 01:53:30 +0800

    fix/lenis-deployment-fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['lenis/dist/lenis.css']  // Add this line to externalize the CSS file
+    }
+  }
 })
